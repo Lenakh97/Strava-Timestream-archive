0c419371b6793b56ec78e171b1f6eca59487bd18
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-03-20 08:06:15 +0100

    feat: add test for getMemberCount()

---
+++ getMemberCount.spec.ts
@@ -0,0 +1,65 @@
+import {
+	CreateTableCommand,
+	DeleteTableCommand,
+	TimestreamWriteClient,
+	WriteRecordsCommand,
+} from '@aws-sdk/client-timestream-write'
+import { randomUUID } from 'crypto'
+import { getMemberCount } from './getMemberCount.js'
+import { stravaToTimestream } from './stravaToTimestream.js'
+import testData from './test-data/activities.json'
+
+const tsw = new TimestreamWriteClient({})
+const testDatabaseName = process.env.TEST_DB_NAME as string
+const testTableName = randomUUID()
+const currentTime = new Date()
+
+beforeAll(async () => {
+	await tsw.send(
+		new CreateTableCommand({
+			DatabaseName: testDatabaseName,
+			TableName: testTableName,
+		}),
+	)
+})
+
+afterAll(async () => {
+	await tsw.send(
+		new DeleteTableCommand({
+			DatabaseName: testDatabaseName,
+			TableName: testTableName,
+		}),
+	)
+})
+
+describe('getMemberCount()', () => {
+	it('should return the member count for the given club', async () => {
+		// Fill Timestream table with test data
+		await tsw.send(
+			new WriteRecordsCommand({
+				DatabaseName: testDatabaseName,
+				TableName: testTableName,
+				Records: stravaToTimestream(42, currentTime, testData),
+			}),
+		)
+		await tsw.send(
+			new WriteRecordsCommand({
+				DatabaseName: testDatabaseName,
+				TableName: testTableName,
+				Records: stravaToTimestream(43, currentTime, testData),
+			}),
+		)
+
+		const expectedMemberCount = {
+			'42': { memberCount: 3 },
+			'43': { memberCount: 3 },
+		}
+
+		expect(
+			await getMemberCount({
+				DatabaseName: testDatabaseName,
+				TableName: testTableName,
+			}),
+		).toEqual(expectedMemberCount)
+	})
+})
---
+++ getMemberCount.ts
@@ -0,0 +1,40 @@
+import {
+	QueryCommand,
+	Row,
+	TimestreamQueryClient,
+} from '@aws-sdk/client-timestream-query'
+
+export type TeamInfo = Record<string, { memberCount: number }>
+
+export const getMemberCount = async ({
+	DatabaseName,
+	TableName,
+}: {
+	DatabaseName: string
+	TableName: string
+}): Promise<TeamInfo> => {
+	const tsq = new TimestreamQueryClient({})
+	const teamInfo = {} as TeamInfo
+	const teams = await tsq.send(
+		new QueryCommand({
+			QueryString: `SELECT DISTINCT Team FROM "${DatabaseName}"."${TableName}"`,
+		}),
+	)
+	const teamArray = []
+	const rows = teams?.Rows as Row[]
+	for (const tsData of rows) {
+		teamArray.push(tsData?.Data?.[0]?.ScalarValue)
+	}
+	for (const TeamID of teamArray) {
+		const result = await tsq.send(
+			new QueryCommand({
+				QueryString: `SELECT COUNT (DISTINCT athlete) FROM "${DatabaseName}"."${TableName}" WHERE Team='${TeamID}'`,
+			}),
+		)
+		teamInfo[TeamID ?? 0] = {
+			memberCount: parseFloat(result?.Rows?.[0]?.Data?.[0]?.ScalarValue ?? '0'),
+		}
+	}
+
+	return teamInfo
+}
