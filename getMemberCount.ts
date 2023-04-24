d8c312cd1ea2f5fa82f8c8d8b229c31d8f216478
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2023-04-24 08:12:38 +0200

    fix: get members from start date for challenge

--- getMemberCount.ts
+++ getMemberCount.ts
@@ -16,7 +16,7 @@ export const getMemberCount = async ({
 	const tsq = new TimestreamQueryClient({})
 	const teams = await tsq.send(
 		new QueryCommand({
-			QueryString: `SELECT COUNT (DISTINCT athlete) as memberCount, Team as teamId FROM "${DatabaseName}"."${TableName}" GROUP BY Team`,
+			QueryString: `SELECT COUNT (DISTINCT athlete) as memberCount, Team as teamId FROM "${DatabaseName}"."${TableName}" WHERE time > '2023-04-17 00:00:00' GROUP BY Team`,
 		}),
 	)
 	return parseResult<{ memberCount: string; teamId: string }>(teams).reduce(
