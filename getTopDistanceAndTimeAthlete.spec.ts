260bb1684d1880e8179c454eab2a44ab6e4c3b38
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2024-04-12 13:06:20 +0200

    feat: add function for getting top athlete distance and time

--- getTopDistanceAndTimeAthlete.spec.ts
+++ getTopDistanceAndTimeAthlete.spec.ts
@@ -5,9 +5,10 @@ import {
 	WriteRecordsCommand,
 } from '@aws-sdk/client-timestream-write'
 import { randomUUID } from 'crypto'
-import { getTopDistanceAthlete } from './getTopDistanceAthlete'
-import { stravaToTimestream } from './stravaToTimestream'
+import { getTopDistanceAndTimeAthlete } from './getTopDistanceAndTimeAthlete.js'
+import { stravaToTimestream } from './stravaToTimestream.js'
 import testData from './test-data/activities2.json'
+import testData2 from './test-data/activities3.json'
 
 const tsw = new TimestreamWriteClient({})
 const testDatabaseName = process.env.TEST_DB_NAME as string
@@ -32,7 +33,7 @@ afterAll(async () => {
 	)
 })
 
-describe('getTopDistanceAthlete()', () => {
+describe('getTopDistanceAndTimeAthlete()', () => {
 	it('should return the total distance', async () => {
 		// Fill Timestream table with test data
 		await tsw.send(
@@ -42,18 +43,35 @@ describe('getTopDistanceAthlete()', () => {
 				Records: stravaToTimestream(42, currentTime, testData),
 			}),
 		)
+		await tsw.send(
+			new WriteRecordsCommand({
+				DatabaseName: testDatabaseName,
+				TableName: testTableName,
+				Records: stravaToTimestream(43, currentTime, testData2),
+			}),
+		)
 
 		const expectedTopDistanceAthlete = {
-			'Alex L.': { distance: 20749.533333333333 },
-			'Eduardo M.': { distance: 17386.966666666667 },
-			'Murat B.': { distance: 6750.8 },
+			'42': {
+				'Alex L.': { distance: 20.749533333333333, time: 4.425555555555556 },
+				'Eduardo M.': { distance: 17.386966666666667, time: 4.418611111111111 },
+				'Murat B.': { distance: 6.7508, time: 1.0438888888888889 },
+			},
+			'43': {
+				'Alex L.': { distance: 20.749533333333333, time: 4.425555555555556 },
+				'Eduardo M.': { distance: 17.386966666666667, time: 4.418611111111111 },
+				'Ola B.': { distance: 6.7508, time: 1.0438888888888889 },
+			},
 		}
 		expect(
-			await getTopDistanceAthlete({
+			await getTopDistanceAndTimeAthlete({
 				DatabaseName: testDatabaseName,
 				TableName: testTableName,
 			}),
 		).toEqual(expectedTopDistanceAthlete)
 	})
 })
-export type AthleteDistanceInfo = Record<string, { distance: number }>
+export type AthleteDistanceInfo = Record<
+	string,
+	{ distance: number; time: number }
+>
