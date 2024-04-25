4e0a53d2b3119b6ec84d7c2c2b40646a8c7d67a4
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2024-04-25 13:38:43 +0200

    refactor: extract startDateString to config

--- lambdas/getMemberCount.ts
+++ lambdas/getMemberCount.ts
@@ -3,7 +3,7 @@ import {
 	TimestreamQueryClient,
 } from '@aws-sdk/client-timestream-query'
 import { parseResult } from '@nordicsemiconductor/timestream-helpers'
-
+import { startDateString } from '../config.js'
 export type TeamInfo = Record<string, { memberCount: number }>
 
 export const getMemberCount = async ({
@@ -16,7 +16,7 @@ export const getMemberCount = async ({
 	const tsq = new TimestreamQueryClient({})
 	const teams = await tsq.send(
 		new QueryCommand({
-			QueryString: `SELECT COUNT (DISTINCT athlete) as memberCount, Team as teamId FROM "${DatabaseName}"."${TableName}" WHERE time > '2023-04-17 00:00:00' GROUP BY Team`,
+			QueryString: `SELECT COUNT (DISTINCT athlete) as memberCount, Team as teamId FROM "${DatabaseName}"."${TableName}" WHERE time > '${startDateString}' GROUP BY Team`,
 		}),
 	)
 	return parseResult<{ memberCount: string; teamId: string }>(teams).reduce(
