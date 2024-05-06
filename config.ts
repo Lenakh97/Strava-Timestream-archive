e58aecc37ff4f098eabd97fc982c810db14e615b
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2024-05-06 12:29:41 +0200

    fix: update headcount

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const StravaChallengeWeeks = [15, 16, 17, 18]
+export const StravaChallengeWeeks = [16, 17, 18, 19]
 export const fallBackStartTimestamp = new Date(
 	'2023-03-27T00:00:00',
 ).toISOString()
@@ -7,14 +7,14 @@ export const distanceGoal = 15726.7 //last years results
 
 //Headcount from spring 2023
 export const officeHeadcount = {
-	'1174164': { memberCount: 301 }, //Finland
-	'1174165': { memberCount: 133 }, //Poland
-	'1174166': { memberCount: 207 }, //Europe
-	'1174167': { memberCount: 237 }, //APAC
-	'1174168': { memberCount: 75 }, //USA
-	'1174140': { memberCount: 492 }, //Trondheim
+	'1174164': { memberCount: 307 }, //Finland
+	'1174165': { memberCount: 109 }, //Poland
+	'1174166': { memberCount: 124 }, //Europe
+	'1174167': { memberCount: 212 }, //APAC
+	'1174168': { memberCount: 72 }, //USA
+	'1174140': { memberCount: 407 }, //Trondheim
 	'1174791': { memberCount: 500 }, //Omega
-	'1174162': { memberCount: 204 }, //Oslo
+	'1174162': { memberCount: 174 }, //Oslo
 }
 /*
 Email to use for sending and receiving random winners 
